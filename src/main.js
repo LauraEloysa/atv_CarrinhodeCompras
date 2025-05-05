c5e97df4db9e29c271d13f5f2fd5df8a4e16b16a
LauraEloysa <lauraeloysad@gmail.com>
2025-05-05 16:49:22 -0300

    FEAT: adicionei o footer #10

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMagnifyingGlass, faCartShopping, faHeart, faUser, faTruck, faStar, faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faCartShopping, faHeart, faUser, faTruck, faStar, faBookOpen, faPhone} from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faMagnifyingGlass, faCartShopping, faHeart, faUser, faRegularHeart, faTruck, faStar, faBookOpen )
+library.add(faMagnifyingGlass, faCartShopping, faHeart, faUser, faRegularHeart, faTruck, faStar, faBookOpen, faPhone )
 
 const app = createApp(App)
 
